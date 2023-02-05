6ea17993e76f16b89af46c8f0021ad4f4fc6c95b
sinavro7 <sinavro7@gmail.com>
2023-02-05 23:29:34 +0900

    application 버전 관리

--- src/main/java/kr/city/eng/pendding/PenddingApplication.java
+++ src/main/java/kr/city/eng/pendding/PenddingApplication.java
@@ -2,12 +2,28 @@ package kr.city.eng.pendding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class PenddingApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(PenddingApplication.class, args);
+  }
+
+  @EventListener(ApplicationReadyEvent.class)
+  public void init(ApplicationReadyEvent event) {
+    ApplicationContext context = event.getApplicationContext();
+
+    Environment env = context.getBean(Environment.class);
+    log.info("app: {}", env.getProperty("info.app.description"));
+    log.info("version: {}", env.getProperty("info.app.version"));
   }
 
 }
